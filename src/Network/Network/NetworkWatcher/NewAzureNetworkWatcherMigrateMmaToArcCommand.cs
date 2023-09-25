14a6e019b4c9dd6d825b399146cb6a82e45a4d01
Sachin Panchal <v-sapanchal@microsoft.com>
2023-09-26 00:53:11 +0530

    ARG query command for LA to AMA

---
+++ src/Network/Network/NetworkWatcher/NewAzureNetworkWatcherMigrateMmaToArcCommand.cs
@@ -0,0 +1,49 @@
+﻿namespace Microsoft.Azure.Commands.Network
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Management.Automation;
+    using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+    using Microsoft.Azure.Commands.Common.Authentication.Abstractions;
+    using Microsoft.Azure.Commands.Common.Authentication;
+    using Microsoft.Azure.Commands.Network.Models;
+    using Microsoft.Azure.Management.Network;
+    using Microsoft.Azure.Management.ResourceGraph;
+    using Microsoft.Azure.Management.ResourceGraph.Models;
+    using Newtonsoft.Json;
+
+    [Cmdlet("New", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "AzureNetworkWatcherMigrateMmaToArc"), OutputType(typeof(PSAzureNetworkWatcherMigrateMmaToArc))]
+    public class NewAzureNetworkWatcherMigrateMmaToArcCommand : NetworkWatcherBaseCmdlet
+    {
+
+        /// <summary>
+        /// Gets or sets the query.
+        /// </summary>s
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipelineByPropertyName = true, HelpMessage = "Resource Graph query")]
+        [AllowEmptyString]
+        public string Query
+        {
+            get;
+            set;
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            this.QueryForArg(this.Query);
+        }
+
+        public void QueryForArg(string query)
+        {
+            ResourceGraphClient rgClient = AzureSession.Instance.ClientFactory.CreateArmClient<ResourceGraphClient>(DefaultContext, AzureEnvironment.Endpoint.ResourceManager);
+            QueryRequest request = new QueryRequest
+            {
+                Query = query
+            };
+            QueryResponse response = rgClient.Resources(request);
+            var data = JsonConvert.DeserializeObject<List<object>>(response.Data.ToString());
+            WriteObject(data);
+        }
+    }
+
+}
