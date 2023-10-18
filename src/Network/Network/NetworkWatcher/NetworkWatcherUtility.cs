2798555d165fa837db3166f22842b1edb308173b
Sachin Panchal <v-sapanchal@microsoft.com>
2023-10-18 11:36:20 +0530

    Commiting all the development code, working on getting resource (CM) for subs collection under user context

---
+++ src/Network/Network/NetworkWatcher/NetworkWatcherUtility.cs
@@ -0,0 +1,106 @@
+﻿using Hyak.Common;
+using Microsoft.Azure.Commands.Common.Authentication.Abstractions;
+using Microsoft.WindowsAzure.Commands.Common;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.Network
+{
+    public class NetworkWatcherUtility
+    {
+        /// <summary>
+        /// Processes the parameters to return a valid resource Id.
+        /// </summary>
+        /// <param name="subscriptionId">The subscription.</param>
+        /// <param name="resourceGroupName">The resource group</param>
+        /// <param name="resourceType">The resource type string in the format: '{providerName}/{typeName}/{nestedTypeName}'</param>
+        /// <param name="resourceName">The resource name in the format: '{resourceName}[/{nestedResourceName}]'</param>
+        /// <param name="extensionResourceType">The extension resource type string in the format: '{providerName}/{typeName}/{nestedTypeName}'</param>
+        /// <param name="extensionResourceName">The extension resource name in the format: '{resourceName}[/{nestedResourceName}]'</param>
+        public static string GetResourceId(Guid? subscriptionId, string resourceGroupName, string resourceType, string resourceName, string extensionResourceType = null, string extensionResourceName = null)
+        {
+            if (subscriptionId == null && !string.IsNullOrWhiteSpace(resourceGroupName))
+            {
+                throw new InvalidOperationException("A resource group cannot be specified without a subscription.");
+            }
+
+            var resourceId = new StringBuilder();
+
+            if (subscriptionId != null)
+            {
+                resourceId.AppendFormat("/subscriptions/{0}", Uri.EscapeDataString(subscriptionId.Value.ToString()));
+            }
+
+            if (!string.IsNullOrWhiteSpace(resourceGroupName))
+            {
+                resourceId.AppendFormat("/resourceGroups/{0}", Uri.EscapeDataString(resourceGroupName));
+            }
+
+            if (!string.IsNullOrWhiteSpace(resourceType))
+            {
+                resourceId.Append(ProcessResourceTypeAndName(resourceType: resourceType, resourceName: resourceName));
+            }
+
+            if (!string.IsNullOrWhiteSpace(extensionResourceType))
+            {
+                resourceId.Append(ProcessResourceTypeAndName(resourceType: extensionResourceType, resourceName: extensionResourceName));
+            }
+
+            return resourceId.ToString();
+        }
+
+        /// <summary>
+        /// Processes a resource type string and a resource name string and 
+        /// </summary>
+        /// <param name="resourceType">The resource type string in the format: '{providerName}/{typeName}/{nestedTypeName}'</param>
+        /// <param name="resourceName">The resource name in the format: '{resourceName}[/{nestedResourceName}]'</param>
+        private static string ProcessResourceTypeAndName(string resourceType, string resourceName)
+        {
+            var resourceId = new StringBuilder();
+            var resourceTypeTokens = resourceType.SplitRemoveEmpty('/');
+            var resourceNameTokens = resourceName.CoalesceString().SplitRemoveEmpty('/');
+
+            resourceId.AppendFormat("/providers/{0}", Uri.EscapeDataString(resourceTypeTokens.First()));
+
+            for (int i = 1; i < resourceTypeTokens.Length; ++i)
+            {
+                resourceId.AppendFormat("/{0}", Uri.EscapeDataString(resourceTypeTokens[i]));
+
+                if (resourceNameTokens.Length > i - 1)
+                {
+                    resourceId.AppendFormat("/{0}", Uri.EscapeDataString(resourceNameTokens[i - 1]));
+                }
+            }
+
+            return resourceId.ToString();
+        }
+
+        /// <summary>
+        /// Creates an <see cref="HttpClient"/>
+        /// </summary>
+        /// <param name="primaryHandlers">The handlers that will be added to the top of the chain.</param>
+        public virtual HttpClient CreateHttpClient(params DelegatingHandler[] primaryHandlers)
+        {
+            var delegateHandlers = new DelegatingHandler[]
+            {
+                new AuthenticationHandler(cloudCredentials: credentials),
+                new UserAgentHandler(headerValues: headerValues),
+                new CmdletInfoHandler(cmdletHeaderValues: cmdletHeaderValues),
+                new TracingHandler(),
+                new RetryHandler(),
+            };
+
+            var pipeline = (HttpMessageHandler)(new HttpClientHandler());
+            var reversedHandlers = primaryHandlers.CoalesceEnumerable().Concat(delegateHandlers).ToArray().Reverse();
+            foreach (var handler in reversedHandlers)
+            {
+                handler.InnerHandler = pipeline;
+                pipeline = handler;
+            }
+
+            return new HttpClient(pipeline);
+        }
+    }
+}
--- src/Network/Network/NetworkWatcher/NewAzureNetworkWatcherMigrateMmaToArcCommand.cs
+++ src/Network/Network/NetworkWatcher/NewAzureNetworkWatcherMigrateMmaToArcCommand.cs
@@ -3,18 +3,42 @@
     using System;
     using System.Collections.Generic;
     using System.Management.Automation;
-    using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
     using Microsoft.Azure.Commands.Common.Authentication.Abstractions;
     using Microsoft.Azure.Commands.Common.Authentication;
     using Microsoft.Azure.Commands.Network.Models;
-    using Microsoft.Azure.Management.Network;
     using Microsoft.Azure.Management.ResourceGraph;
     using Microsoft.Azure.Management.ResourceGraph.Models;
     using Newtonsoft.Json;
+    using System.Linq;
+    using System.Security;
+    using Microsoft.Azure.Commands.Profile.Models;
+    using Microsoft.Azure.Commands.Common.Authentication.Models;
+    using Microsoft.Azure.Commands.ResourceManager.Common;
+    using Microsoft.Azure.Commands.Common.Authentication.ResourceManager;
+    using Newtonsoft.Json.Linq;
+    using System.Threading.Tasks;
+    using System.Threading;
+    using System.Text;
+    using System.Net.Http;
 
     [Cmdlet("New", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "AzureNetworkWatcherMigrateMmaToArc"), OutputType(typeof(PSAzureNetworkWatcherMigrateMmaToArc))]
     public class NewAzureNetworkWatcherMigrateMmaToArcCommand : NetworkWatcherBaseCmdlet
     {
+        public Action<string> WarningLog;
+        private IAzureTokenCache _cache;
+        private IProfileOperations _profile;
+
+        public ISubscriptionClientWrapper SubscriptionAndTenantClient = null;
+        
+        /// <summary>
+        /// The endpoint that this client will communicate with.
+        /// </summary>
+        public Uri EndpointUri { get; set; }
+
+        /// <summary>
+        /// The azure http client wrapper to use.
+        /// </summary>
+        private readonly HttpClientHelper httpClientHelper;
 
         /// <summary>
         /// Gets or sets the query.
@@ -27,9 +51,35 @@
             set;
         }
 
+        ///// <summary>
+        ///// Gets or sets the Work Space Id.
+        ///// </summary>s
+        //[Parameter(Mandatory = true, Position = 0, ValueFromPipelineByPropertyName = true, HelpMessage = "Work Space Id")]
+        //[AllowEmptyString]
+        //public string WorkSpaceId
+        //{
+        //    get;
+        //    set;
+        //}
+
         public override void Execute()
         {
             base.Execute();
+            _cache = AzureSession.Instance.TokenCache;
+            _profile = AzureRmProfileProvider.Instance.GetProfile<AzureRmProfile>();
+            var profileClient = new RMProfileClient(_profile);
+            SubscriptionAndTenantClient = profileClient.SubscriptionAndTenantClient;
+            var endpoint = DefaultContext.Environment.GetEndpoint(AzureEnvironment.Endpoint.ResourceManager);
+            if (string.IsNullOrWhiteSpace(endpoint))
+            {
+                throw new ApplicationException(
+                    "The endpoint for the Azure Resource Manager service is not set. Please report this issue via GitHub or contact Microsoft customer support.");
+            }
+
+            var endpointUri = new Uri(endpoint, UriKind.Absolute);
+            EndpointUri = endpointUri;
+
+            GetAllSubscriptionsByUserContext();
             this.QueryForArg(this.Query);
         }
 
@@ -43,6 +93,354 @@
             QueryResponse response = rgClient.Resources(request);
             var data = JsonConvert.DeserializeObject<object>(response.Data.ToString());
             WriteObject(data);
+        }
+        public void GetAllSubscriptionsByUserContext()
+        {
+            var tenantId = DefaultContext.Tenant.Id;
+            IEnumerable<AzureSubscription> subscriptionsList = ListAllSubscriptionsForTenant(tenantId);
+        }
+
+        private IAccessToken AcquireAccessToken(
+           IAzureAccount account,
+           IAzureEnvironment environment,
+           string tenantId,
+           SecureString password,
+           string promptBehavior,
+           Action<string> promptAction,
+           string resourceId = AzureEnvironment.Endpoint.ActiveDirectoryServiceEndpointResourceId)
+        {
+            if (account.Type == AzureAccount.AccountType.AccessToken)
+            {
+                tenantId = tenantId ?? account.GetCommonTenant();
+                return new SimpleAccessToken(account, tenantId);
+            }
+
+            return AzureSession.Instance.AuthenticationFactory.Authenticate(
+                account,
+                environment,
+                tenantId,
+                password,
+                promptBehavior,
+                promptAction,
+                _cache,
+                resourceId);
+        }
+
+
+        private IEnumerable<AzureSubscription> ListAllSubscriptionsForTenant(
+          string tenantId)
+        {
+            IAzureAccount account = _profile.DefaultContext.Account;
+            IAzureEnvironment environment = _profile.DefaultContext.Environment;
+            SecureString password = null;
+            string promptBehavior = ShowDialog.Never;
+            IAccessToken accessToken = null;
+            try
+            {
+                accessToken = AcquireAccessToken(account, environment, tenantId, password, promptBehavior, null);
+            }
+            catch (Exception e)
+            {
+                WriteWarningMessage(e.Message);
+                //WriteDebugMessage(string.Format(ProfileMessages.UnableToAqcuireToken, tenantId, e.ToString()));
+                return new List<AzureSubscription>();
+            }
+
+            return SubscriptionAndTenantClient?.ListAllSubscriptionsForTenant(accessToken, account, environment);
+        }
+
+        private void WriteWarningMessage(string message)
+        {
+            if (WarningLog != null)
+            {
+                WarningLog(message);
+            }
+        }
+
+        //public List<AzureTenant> ListTenants(string tenant = "")
+        //{
+        //    IList<AzureTenant> tenants = ListAccountTenants(DefaultContext.Account, DefaultContext.Environment, null, ShowDialog.Never, null);
+        //    return tenants.Where(t => string.IsNullOrEmpty(tenant) ||
+        //                                 tenant.Equals(t.Id.ToString(), StringComparison.OrdinalIgnoreCase) ||
+        //                                 Array.Exists(t.GetPropertyAsArray(AzureTenant.Property.Domains), e => tenant.Equals(e, StringComparison.OrdinalIgnoreCase)))
+        //                         .ToList();
+        //}
+
+        //private List<AzureTenant> ListAccountTenants(
+        //  IAzureAccount account,
+        //  IAzureEnvironment environment,
+        //  SecureString password,
+        //  string promptBehavior,
+        //  Action<string> promptAction)
+        //{
+        //    IList<AzureTenant> result = new List<AzureTenant>();
+        //    var commonTenant = account.GetCommonTenant();
+        //    try
+        //    {
+        //        var commonTenantToken = AcquireAccessToken(
+        //            account,
+        //            environment,
+        //            commonTenant,
+        //            password,
+        //            promptBehavior,
+        //            promptAction);
+
+        //        result =  SubscriptionAndTenantClient?.ListAccountTenants(commonTenantToken, environment);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        WriteWarningMessage(string.Format(ProfileMessages.UnableToAqcuireToken, commonTenant, e.Message));
+        //        WriteDebugMessage(string.Format(ProfileMessages.UnableToAqcuireToken, commonTenant, e.ToString()));
+        //        if (account.IsPropertySet(AzureAccount.Property.Tenants))
+        //        {
+        //            result =
+        //                account.GetPropertyAsArray(AzureAccount.Property.Tenants)
+        //                    .Select(ti =>
+        //                    {
+        //                        var tenant = new AzureTenant();
+
+        //                        Guid guid;
+        //                        if (Guid.TryParse(ti, out guid))
+        //                        {
+        //                            tenant.Id = ti;
+        //                        }
+        //                        else
+        //                        {
+        //                            tenant.Directory = ti;
+        //                        }
+
+        //                        return tenant;
+        //                    }).ToList();
+        //        }
+        //        if (!result.Any())
+        //        {
+        //            throw;
+        //        }
+
+        //    }
+
+        //    return result.ToList();
+        //}
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void GetConnectionMonitorHasMMAMachineEndpoint(IEnumerable<AzureSubscription> subscriptions, string workSpaceId)
+        {
+
+        }
+
+        /// <summary>
+        /// Gets the resources in a subscription.
+        /// </summary>
+        private async Task<ResponseWithContinuation<JObject[]>> ListResourcesInSubscription()
+        {
+            var filterQuery = QueryFilterBuilder
+                .CreateFilter(
+                    subscriptionId: null,
+                    resourceGroup: null,
+                    resourceType: this.ResourceType,
+                    resourceName: this.Name,
+                    tagName: null,
+                    tagValue: null,
+                    filter: this.ODataQuery);
+
+            return await this
+                .GetResourcesClient()
+                .ListResources<JObject>(
+                    subscriptionId: this.SubscriptionId.Value,
+                    apiVersion: this.DefaultApiVersion,
+                    filter: filterQuery,
+                    cancellationToken: this.CancellationToken.Value)
+                .ConfigureAwait(continueOnCapturedContext: false);
+        }
+
+        ///// <summary>
+        ///// Gets a new instance of the <see cref="ResourceManagerRestRestClient"/>.
+        ///// </summary>
+        //public ResourceManagerRestRestClient GetResourcesClient()
+        //{
+        //    var endpoint = DefaultContext.Environment.GetEndpoint(AzureEnvironment.Endpoint.ResourceManager);
+
+        //    if (string.IsNullOrWhiteSpace(endpoint))
+        //    {
+        //        throw new ApplicationException(
+        //            "The endpoint for the Azure Resource Manager service is not set. Please report this issue via GitHub or contact Microsoft customer support.");
+        //    }
+
+        //    var endpointUri = new Uri(endpoint, UriKind.Absolute);
+
+        //    return new ResourceManagerRestRestClient(
+        //        endpointUri: endpointUri,
+        //        httpClientHelper: HttpClientHelperFactory.Instance
+        //        .CreateHttpClientHelper(
+        //                credentials: AzureSession.Instance.AuthenticationFactory
+        //                                         .GetServiceClientCredentials(
+        //                                            DefaultContext,
+        //                                            AzureEnvironment.Endpoint.ResourceManager),
+        //                headerValues: AzureSession.Instance.ClientFactory.UserAgents,
+        //                cmdletHeaderValues: this.GetCmdletHeaders()));
+        //}
+
+        /// <summary>
+        /// Lists all the resources under a single the subscription.
+        /// </summary>
+        /// <param name="subscriptionId">The subscription Id.</param>
+        /// <param name="apiVersion">The API version to use.</param>
+        /// <param name="cancellationToken"></param>
+        /// <param name="top">The number of resources to fetch.</param>
+        /// <param name="filter">The filter query.</param>
+        public Task<ResponseWithContinuation<TType[]>> ListResources<TType>(
+            Guid subscriptionId,
+            string apiVersion,
+            CancellationToken cancellationToken,
+            int? top = null,
+            string filter = null)
+        {
+            var resourceId = NetworkWatcherUtility.GetResourceId(
+                subscriptionId: subscriptionId,
+                resourceGroupName: null,
+                resourceType: null,
+                resourceName: null);
+
+            var requestUri = this.GetResourceManagementRequestUri(
+                resourceId: resourceId,
+                action: "resources",
+                top: top == null ? null : string.Format("$top={0}", top.Value),
+                odataQuery: string.IsNullOrWhiteSpace(filter) ? null : string.Format("$filter={0}", filter),
+                apiVersion: apiVersion);
+
+            return this.SendRequestAsync<ResponseWithContinuation<TType[]>>(
+                httpMethod: HttpMethod.Get,
+                requestUri: requestUri,
+                cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// Generates a resource management request <see cref="Uri"/> based on the input parameters. Supports both subscription and tenant level resource and the condensed resource type and name format.
+        /// </summary>
+        /// <param name="resourceId">The resource Id.</param>
+        /// <param name="apiVersion">The API version.</param>
+        /// <param name="action">The action.</param>
+        /// <param name="odataQuery">The OData query.</param>
+        /// <param name="top">Top resources - used in list queries.</param>
+        public Uri GetResourceManagementRequestUri(
+            string resourceId,
+            string apiVersion,
+            string action = null,
+            string odataQuery = null,
+            string top = null)
+        {
+            var resourceIdStringBuilder = new StringBuilder(resourceId.CoalesceString().TrimEnd('/'));
+
+            if (!string.IsNullOrWhiteSpace(action))
+            {
+                resourceIdStringBuilder.AppendFormat("/{0}", action);
+            }
+
+            var parts = new[]
+            {
+                top,
+                odataQuery,
+                string.Format("api-version={0}", apiVersion)
+            };
+
+            var queryString = parts.Where(part => !string.IsNullOrWhiteSpace(part)).ConcatStrings("&");
+
+            resourceIdStringBuilder.AppendFormat("?{0}", queryString);
+
+            var relativeUri = resourceIdStringBuilder.ToString()
+                .Select(character => char.IsWhiteSpace(character) ? "%20" : character.ToString())
+                .ConcatStrings();
+
+            return new Uri(baseUri: this.EndpointUri, relativeUri: relativeUri);
+        }
+
+        /// <summary>
+        /// Sends an HTTP request message and returns the result.
+        /// </summary>
+        /// <typeparam name="TResponseType">The type of the result of response from the server.</typeparam>
+        /// <param name="httpMethod">The http method to use.</param>
+        /// <param name="requestUri">The Uri of the operation.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        protected async Task<TResponseType> SendRequestAsync<TResponseType>(
+            HttpMethod httpMethod,
+            Uri requestUri,
+            CancellationToken cancellationToken)
+        {
+            using (var response = await this
+                .SendRequestAsync(httpMethod: httpMethod, requestUri: requestUri, cancellationToken: cancellationToken)
+                .ConfigureAwait(continueOnCapturedContext: false))
+            {
+                return await response
+                    .ReadContentAsJsonAsync<TResponseType>()
+                    .ConfigureAwait(continueOnCapturedContext: false);
+            }
+        }
+
+        /// <summary>
+        /// Sends an HTTP request message and returns the result.
+        /// </summary>
+        /// <param name="httpMethod">The http method to use.</param>
+        /// <param name="requestUri">The Uri of the operation.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        protected async Task<HttpResponseMessage> SendRequestAsync(
+            HttpMethod httpMethod,
+            Uri requestUri,
+            CancellationToken cancellationToken)
+        {
+            using (var request = new HttpRequestMessage(method: httpMethod, requestUri: requestUri))
+            {
+                return await this.SendRequestAsync(request: request, cancellationToken: cancellationToken)
+                    .ConfigureAwait(continueOnCapturedContext: false);
+            }
+        }
+
+        /// <summary>
+        /// Sends an HTTP request message and returns the result.
+        /// </summary>
+        /// <param name="request">The http request to send.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        protected async Task<HttpResponseMessage> SendRequestAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            using (var httpClient = this.httpClientHelper.CreateHttpClient())
+            {
+                try
+                {
+                    var response = await httpClient
+                        .SendAsync(request: request, cancellationToken: cancellationToken)
+                        .ConfigureAwait(continueOnCapturedContext: false);
+
+                    if (!response.StatusCode.IsSuccessfulRequest())
+                    {
+                        var errorResponse = await ResourceManagerRestClientBase
+                            .TryReadErrorResponseMessage(response, rewindContentStream: true)
+                            .ConfigureAwait(continueOnCapturedContext: false);
+
+                        var message = await ResourceManagerRestClientBase
+                            .GetErrorMessage(request: request, response: response, errorResponse: errorResponse)
+                            .ConfigureAwait(continueOnCapturedContext: false);
+
+                        throw new ErrorResponseMessageException(
+                            httpStatus: response.StatusCode,
+                            errorResponseMessage: errorResponse,
+                            errorMessage: message);
+                    }
+
+                    return response;
+                }
+                catch (Exception exception)
+                {
+                    if (exception is OperationCanceledException && !cancellationToken.IsCancellationRequested)
+                    {
+                        throw new Exception(ProjectResources.OperationFailedWithTimeOut);
+                    }
+
+                    throw;
+                }
+            }
         }
     }
 
